80d6231ba01511be93e4a638a6687463e04e6850
dpcheng1104 <272395703@qq.com>
2021-12-21 10:10:26 +0800

    getandpost

---
+++ Junit4Test/src/test/java/testcase/Junit4_01_TestJavaTest.java
@@ -0,0 +1,11 @@
+import junit.framework.TestCase;
+
+/**
+ *@Package testcase
+ *@Author   Kevin
+ *@date     2021/12/5   12:01
+ *@version  v1.0
+ *@Copyright    @
+**/public class Junit4_01_TestJavaTest extends TestCase {
+  
+}
---
+++ TestAllureReport/src/main/java/DemoTest.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+/**
+ *@Package PACKAGE_NAME
+ *@Author   Kevin
+ *@date     2021/12/11   11:20
+ *@version  v1.0
+ *@Copyright    @
+**/public class DemoTest {
+}
